8bcea4a27bf42bc8d8e54b21f43f73632726a21c
mahdiputra <mahdip.putra@gmail.com>
2018-02-27 18:41:33 +0700

    tugas weekly 2

---
+++ exercise-1.js
@@ -0,0 +1,24 @@
+var nama = 'zero';
+var peran = 'Penyihir';
+
+if (nama == 'Mikael' && peran == '' ){
+  console.log("Halo Mikael, Pilih peranmu untuk memulai game!");
+} 
+else if (nama == 'Nina' && peran == 'ksatria')
+{  console.log("Selamat datang di Dunia Proxytia, Nina");console.log("Halo Ksatria Nina, kamu dapat menyerang dengan senjatamu!.");
+} 
+
+else if (nama == 'danu' && peran == 'tabib')
+{  console.log("Selamat datang di Dunia Proxytia, Danu");console.log("Halo Tabib Danu, kamu akan membantu temanmu yang terluka.");
+} 
+
+else if (nama == 'zero' && peran == 'Penyihir')
+{  console.log("Selamat datang di Dunia Proxytia, Zero"); console.log("Halo Penyihir Zero, ciptakan keajaiban yang membantu kemenanganmu!");
+} 
+
+else  {
+  console.log("Nama harus diisi!");
+  }
+
+
+
